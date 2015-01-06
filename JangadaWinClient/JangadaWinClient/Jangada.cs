dfec63df4180d775da72652321e81d087f34d87c
Caio Keto <caionmoreno1@gmail.com>
2015-01-06 10:58:26 -0200

    initial commit

---
+++ JangadaWinClient/JangadaWinClient/Jangada.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using TomShane.Neoforce.Controls;
+
+namespace JangadaWinClient
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Jangada : Microsoft.Xna.Framework.Game
+    {
+        private static Jangada instance;
+
+        public static Jangada getInstance()
+        {
+            return instance;
+        }
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Manager manager;
+        MainMenu mainMenu;
+        LoginWindow loginWindow;
+        TomShane.Neoforce.Controls.Console consoleWindow;
+        Texture2D background;
+        GraphicsDevice device;
+
+        int screenWidth;
+        int screenHeight;
+
+        public Jangada()
+        {
+            instance = this;
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            manager = new Manager(this, graphics);
+            manager.Initialize();
+            manager.AutoCreateRenderTarget = false;
+            base.Initialize();
+        }
+
+
+        void loginBtn_click(object sender, TomShane.Neoforce.Controls.EventArgs e)
+        {
+            loginWindow.Show();
+        }
+
+        public void addWindow()
+        {
+            consoleWindow = new TomShane.Neoforce.Controls.Console(manager);
+            consoleWindow.Top = 320;
+            consoleWindow.Width = 400;
+            consoleWindow.Init();
+            consoleWindow.TextBoxVisible = false;
+            consoleWindow.Name = "ConsoleWindow";
+            manager.Add(consoleWindow);
+            consoleWindow.Channels.Add(new ConsoleChannel(0, "DebugMsg", Color.Yellow));
+            mainMenu = new MainMenu(manager);
+            mainMenu.Init();
+            Button loginBtn = new Button(manager);
+            loginBtn.Init();
+            loginBtn.Width = 100;
+            loginBtn.Text = "Login";
+            loginBtn.Click += new TomShane.Neoforce.Controls.EventHandler(this.loginBtn_click);
+            mainMenu.Add(loginBtn);
+            mainMenu.Width = 100;
+            manager.Add(mainMenu);
+            loginWindow = new LoginWindow(manager);
+            loginWindow.Init();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            device = graphics.GraphicsDevice;
+            background = Content.Load<Texture2D>("background");
+            addWindow();
+            screenWidth = device.PresentationParameters.BackBufferWidth;
+            screenHeight = device.PresentationParameters.BackBufferHeight;
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            // Allows the game to exit
+            if (state.IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // TODO: Add your update logic here
+            manager.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            manager.BeginDraw(gameTime);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            DrawScenery();
+            spriteBatch.End();
+            
+            manager.EndDraw();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawScenery()
+        {
+            Rectangle screenRectangle = new Rectangle(0, 0, screenWidth, screenHeight);
+            spriteBatch.Draw(background, screenRectangle, Color.White);
+        }
+    }
+}
---
+++ JangadaWinClient/JangadaWinClient/LoginWindow.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TomShane.Neoforce.Controls;
+
+namespace JangadaWinClient
+{
+    public class LoginWindow
+    {
+        Window loginWindow;
+        Manager manager;
+        TextBox loginBox;
+        TextBox passBox;
+        TomShane.Neoforce.Controls.Console consoleWindow;
+
+        public LoginWindow(Manager manager)
+        {
+            this.manager = manager;
+            consoleWindow = (TomShane.Neoforce.Controls.Console)manager.GetControl("ConsoleWindow");
+        }
+
+        public void Init()
+        {
+            loginWindow = new Window(manager);
+            loginWindow.Init();
+            loginWindow.Text = "Login";
+            loginWindow.Width = 250;
+            loginWindow.Height = 250;
+            loginWindow.Center();
+
+            Label loginLbl = new Label(manager);
+            loginLbl.Init();
+            loginLbl.Text = "User:";
+            loginLbl.Top = 10;
+            loginLbl.Left = 10;
+
+            loginBox = new TextBox(manager);
+            loginBox.Init();
+            loginBox.Top = 30;
+            loginBox.Left = 10;
+            loginBox.Width = 200;
+
+            Label passLbl = new Label(manager);
+            passLbl.Init();
+            passLbl.Text = "Password:";
+            passLbl.Top = 60;
+            passLbl.Left = 10;
+
+            passBox = new TextBox(manager);
+            passBox.Init();
+            passBox.Top = 80;
+            passBox.Left = 10;
+            passBox.Width = 200;
+            passBox.Mode = TextBoxMode.Password;
+
+            Button enterBtn = new Button(manager);
+            enterBtn.Init();
+            enterBtn.Top = 120;
+            enterBtn.Left = 70;
+            enterBtn.Width = 100;
+            enterBtn.Text = "Login";
+            enterBtn.Click += new TomShane.Neoforce.Controls.EventHandler(this.loginBtn_click);
+
+            loginWindow.Add(enterBtn);
+            loginWindow.Add(loginLbl);
+            loginWindow.Add(loginBox);
+            loginWindow.Add(passLbl);
+            loginWindow.Add(passBox);
+
+            manager.Add(loginWindow);
+            loginWindow.Hide();
+        }
+
+        public void Show()
+        {
+            loginWindow.Show();
+            loginBox.Focused = true;
+            
+        }
+
+        public void Hide()
+        {
+            loginWindow.Hide();
+        }
+
+        private void loginBtn_click(object sender, TomShane.Neoforce.Controls.EventArgs e)
+        {
+            consoleWindow.MessageBuffer.Add(new ConsoleMessage("Teste", "Attempting to login...", 0));
+            consoleWindow.MessageBuffer.Add(new ConsoleMessage("Teste", "User: " + loginBox.Text + " Pass: " + passBox.Text, 0));
+        }
+    }
+}
